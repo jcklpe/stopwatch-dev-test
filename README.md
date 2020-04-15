89b49b738324d4f6f1dc59df04d9446b6ef407cf
jcklpe <david@jackalope.tech>
2020-04-14 20:06:58 -0500

    updated readme with notes

--- README.md
+++ README.md
@@ -1,29 +1,50 @@
 # UI Exercise
 
-Using only HTML, CSS and plain Javascript, create a Stopwatch application that meets _at least_ the following criteria:
 
-- Supports a start button/feature, which begins the clock.
 
-- Supports a pause button/feature, which pauses the clock.
+Hi,
 
-- Supports a resume button/feature, which resumes a paused clock.
+Thank you for checking out my project. Let's start with the project's challenge prompt that I received:
 
-- Supports a reset button/feature, which resets the time.
+> Using only HTML, CSS and plain Javascript, create a Stopwatch application that meets _at least_ the following criteria:
+> -  Supports a start button/feature, which begins the clock.
 
-- Supports being insert on a page multiple times, i.e. 3 or more separate clocks should be able to run independently at once.
+check! ✅
+> - Supports a pause button/feature, which pauses the clock.
 
-- Make sure it is responsive.
+check! ✅
+> - Supports a resume button/feature, which resumes a paused clock.
 
-### Pointers:
+check! ✅
+> - Supports a reset button/feature, which resets the time.
 
-- Focus on a complete solution, and high quality.
+check! ✅
+> - Supports being insert on a page multiple times, i.e. 3 or more separate clocks should be able to run independently at once.
 
-- Demonstrate your knowledge of Object Oriented principals.
+I didn't actually hit the mark on this part of the project. I'm still learning, and while I know that this probably has to do with object factories/constructors I'm not super familar with that pattern. So I'm taking a crack at it tonight but not sure if it will be done before you review this project. I've created an alt-index html and index file though to experiment with though.
 
-- Demonstrate your knowledge of CSS.
+> - Make sure it is responsive.
 
-- Try to avoid using libraries, but don’t let that impede your ability to provide a working solution.
+check! ✅
 
-### Extra Credit:
+> ### Pointers:
+> - Focus on a complete solution, and high quality.
 
-- Wow us with your CSS skills
+I hope I hit the mark here. I made a few choices that I think hopefully serve to improve the completeness and quality of the code.
+
+1. I used `getAnimationFrame` since it's supposed to have better performance than setInterval
+2. I based the timer off a comparison of Unix Epoch time instead of just using an incrementer as apparently both getAnimationFrame and setInterval aren't super accurate and that can lead to some millisecond drift.
+
+> - Demonstrate your knowledge of Object Oriented principals.
+
+I honestly have more exposure to functional programming. I'm pretty much self taught and that's what's "hip" right now so as a result it's what I've been exposed to. Pretty sure that's why I had trouble with the multiple components on page problem though so I'm reading up.
+
+> - Demonstrate your knowledge of CSS.
+
+I kept things pretty simple. but hopefully my design sense shows through.
+
+> - Try to avoid using libraries, but don’t let that impede your ability to provide a working solution.
+
+I did not use any libraries. I have use relied heavily on frameworks and libraries so I've been consciously trying to focus on vanilla js and get away from that crutch. I did use the parcel bundler for scss and babel transpilation. First time using it! Way less overhead compared to webpack. I like it.
+
+To run the app just run `npm install` and then `npm run start`. You can run the alt-index using `npm run alt`.
---
+++ src/alt-index.js
@@ -0,0 +1,141 @@
+//- scss injection
+import "./styles.scss";
+
+//ui selector variables
+const timerDisplay = document.querySelector(`.time-display`);
+const toggleButton = document.querySelector(`.toggle-timer`);
+const resetButton = document.querySelector(`.reset-timer`);
+
+// declare initial time variables
+let now = 0;
+let startTime = 0;
+let timeElapsed = 0;
+let pausedTime = 0;
+let unpausedTime = 0;
+let pauseIncrement = 0;
+const totalPausedTimeArray = [];
+let totalPausedTime = 0;
+let seconds = 0;
+let minutes = 0;
+
+// declare initial display content
+let content = `0:00.00`;
+
+// declare initial isState
+let isInitialState = toggleButton.classList.contains(`initial-state`);
+let isRunning = toggleButton.classList.contains(`running`);
+let isPaused = toggleButton.classList.contains(`paused`);
+
+// utility functions
+const sumArray = function(array) {
+  return array.reduce(function(a, b) {
+    return a + b;
+  }, 0);
+};
+
+// stopwatch loop
+function loop() {
+  // update ui state
+  isInitialState = toggleButton.classList.contains(`initial-state`);
+  isRunning = toggleButton.classList.contains(`running`);
+  isPaused = toggleButton.classList.contains(`paused`);
+
+  //update time variables
+  now = Date.now();
+
+  if (isRunning) {
+    timeElapsed = now - startTime - totalPausedTime;
+    seconds = timeElapsed / 1000;
+    seconds = (seconds % 60).toFixed(2);
+    minutes = Math.floor(timeElapsed / 1000 / 60);
+  }
+
+  // render display content
+  if (seconds < 9.99) {
+    content = `${minutes}:0${seconds}`;
+  } else {
+    content = `${minutes}:${seconds}`;
+  }
+
+  if (!isInitialState) {
+    document.title = content;
+    timerDisplay.innerHTML = content;
+  } else {
+    content = `0:00.00`;
+    document.title = `Stopwatch`;
+    timerDisplay.innerHTML = content;
+  }
+
+  requestAnimationFrame(loop);
+}
+
+//- button functions
+// toggle time between 3 states of initial, running, and paused
+function toggleTimer() {
+  switch (true) {
+    // if in intial state, start timer to run state, remove initial state, and log start time
+    case isInitialState:
+      startTime = Date.now();
+      toggleButton.classList.add(`running`);
+      toggleButton.classList.remove(`initial-state`);
+      toggleButton.textContent = `Pause it`;
+
+      break;
+
+    // if timer is currently running, pause timer, remove running, and save time to variable
+    case isRunning:
+      toggleButton.classList.add(`paused`);
+      toggleButton.classList.remove(`running`);
+      toggleButton.textContent = `Start`;
+      pausedTime = Date.now();
+
+      break;
+
+    // if timer is currently paused then start running state, remove paused, and update time variables
+    case isPaused:
+      toggleButton.classList.add(`running`);
+      toggleButton.classList.remove(`paused`);
+      toggleButton.textContent = `Pause`;
+      unpausedTime = Date.now();
+      pauseIncrement = unpausedTime - pausedTime;
+      totalPausedTimeArray.push(pauseIncrement);
+      totalPausedTime = sumArray(totalPausedTimeArray);
+
+      break;
+
+    default:
+  }
+}
+
+//-
+
+// revert to intitial state, and reset time variables
+function resetTimer() {
+  // reset to initial state
+  toggleButton.classList.remove(`running`);
+  toggleButton.classList.remove(`paused`);
+  toggleButton.classList.add(`initial-state`);
+
+  // reset time variables
+  startTime = 0;
+  timeElapsed = 0;
+  pausedTime = 0;
+  unpausedTime = 0;
+  pauseIncrement = 0;
+  totalPausedTimeArray.length = 0;
+  totalPausedTime = 0;
+  seconds = 0;
+  minutes = 0;
+
+  // reset ui content
+  toggleButton.textContent = `Start`;
+  content = `0:00.00`;
+}
+
+//- button event listeners
+toggleButton.addEventListener(`click`, toggleTimer);
+resetButton.addEventListener(`click`, resetTimer);
+
+//- update time
+
+requestAnimationFrame(loop);
